64338fa3e8c7aef5aff6fcd3871eecd394d86a25
boubast <calbast@gmail.com>
2025-09-26 16:06:28 +0200

    Récupération des bon fichiers

--- booking/booking.py
+++ booking/booking.py
@@ -1,112 +1,24 @@
-from flask import Flask, render_template, request, jsonify, make_response
-import requests
-import json
-from werkzeug.exceptions import NotFound
-import os
+from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
+from flask import Flask, request, jsonify
 
+import resolvers as r
+
+PORT = 3001
+HOST = '0.0.0.0'
 app = Flask(__name__)
 
-PORT = 3201
-HOST = '0.0.0.0'
+# todo create elements for Ariadne
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-with open(f'{script_dir}/databases/bookings.json', "r") as jsf:
-    bookings = json.load(jsf)["bookings"]
-
-
-def write(booking):
-    with open(f'{script_dir}/databases/bookings.json', 'w') as f:
-        full = {}
-        full['bookings'] = bookings
-        json.dump(full, f)
-
-
-def auth(req_body, url_userid):
-    if not req_body or "user_id" not in req_body:
-        return make_response(jsonify({"error": "Authenticaiton failed"}), 400)
-
-    try:
-        resp = requests.get(f"http://localhost:3203/users/{req_body['user_id']}")
-    except Exception as e:
-        return make_response(jsonify({"error": "Users service unavailable", "detail": str(e)}), 503)
-
-    if resp.status_code != 200:
-        return make_response(jsonify({"error": "Invalid user_id"}), 401)
-
-    payload = resp.json()
-    is_admin = bool(payload.get("is_admin"))
-
-    if not is_admin and str(req_body["user_id"]) != str(url_userid):
-        return make_response(jsonify({"error": "Forbidden"}), 403)
-
-    return None  # authorized
-
-
-@app.route("/booking/<userid>", methods=['GET'])
-def get_booking_by_userid(userid):
-    req_body = request.get_json()
-    is_auth = auth(req_body, userid)
-    if is_auth is not None:
-        return is_auth
-
-    for booking in bookings:
-        if str(booking["userid"]) == userid:
-            res = make_response(jsonify(booking), 200)
-            return res
-    return make_response(jsonify({"error": "User id not found"}), 500)
-
-
-@app.route("/booking", methods=['POST'])
-def add_booking():
-    req = request.get_json()
-    for booking in bookings:
-        if str(booking["userid"]) == str(req["userid"]):
-            return make_response(jsonify({"error": "Bookings for this user already exists"}), 500)
-
-    try:
-        resp = requests.get(f"http://localhost:3203/users/{req["userid"]}")
-        if resp.status_code != 200:
-            return make_response(jsonify({"error": f"Invalid user ID: {req["userid"]}"}), 500)
-    except Exception as e:
-        return make_response(jsonify({"error": "Users service unavailable", "detail": str(e)}), 503)
-
-    try:
-        for dates in req["dates"]:
-            resp = requests.get(f"http://localhost:3202/schedule/{dates["date"]}")
-            if resp.status_code != 200:
-                return make_response(jsonify({"error": f"No movies avalaible for this date: {dates["date"]}"}), 500)
-            for movie in dates["movies"]:
-                if movie not in resp.json().get("movies", []):
-                    return make_response(
-                        jsonify({"error": f"Movie {movie} not available for this date: {dates['date']}"}), 500)
-    except Exception as e:
-        return make_response(jsonify({"error": "Schedule service unavailable", "detail": str(e)}), 503)
-
-    bookings.append(req)
-    write(bookings)
-    return make_response(jsonify({"message": "Booking added"}), 200)
-
-
-@app.route("/booking/<userid>", methods=['DELETE'])
-def delete_schedule(userid):
-    req_body = request.get_json()
-    is_auth = auth(req_body, userid)
-    if is_auth is not None:
-        return is_auth
-
-    for booking in bookings:
-        if str(booking["userid"]) == str(userid):
-            bookings.remove(booking)
-            write(bookings)
-            return make_response(jsonify(booking), 200)
-    return make_response(jsonify({"error": "User id not found"}), 500)
-
-
+# root message
 @app.route("/", methods=['GET'])
 def home():
-    return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
+    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
 
+# graphql entry points
+@app.route('/graphql', methods=['POST'])
+def graphql_server():
+    # todo to complete
 
 if __name__ == "__main__":
-    print("Server running in port %s" % (PORT))
+    print("Server running in port %s"%(PORT))
     app.run(host=HOST, port=PORT)
--- schedule/schedule.py
+++ schedule/schedule.py
@@ -1,74 +1,22 @@
-import requests
-from flask import Flask, render_template, request, jsonify, make_response
+import grpc
+from concurrent import futures
+import schedule_pb2
+import schedule_pb2_grpc
 import json
-from werkzeug.exceptions import NotFound
-import os
 
-app = Flask(__name__)
+class ScheduleServicer(schedule_pb2_grpc.ScheduleServicer):
 
-PORT = 3202
-HOST = '0.0.0.0'
+    def __init__(self):
+        with open('{}/data/times.json'.format("."), "r") as jsf:
+            self.db = json.load(jsf)["schedule"]
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-with open(f'{script_dir}/databases/times.json', "r") as jsf:
-    schedule = json.load(jsf)["schedule"]
-
-
-def write(schedule):
-    with open(f'{script_dir}/databases/times.json', 'w') as f:
-        full = {}
-        full['schedule'] = schedule
-        json.dump(full, f)
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    schedule_pb2_grpc.add_ScheduleServicer_to_server(ScheduleServicer(), server)
+    server.add_insecure_port('[::]:3002')
+    server.start()
+    server.wait_for_termination()
 
 
-@app.route("/", methods=['GET'])
-def home():
-    return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
-
-
-@app.route("/schedule/<date>", methods=['GET'])
-def get_schedule_bydate(date):
-    for day in schedule:
-        if str(day["date"]) == str(date):
-            res = make_response(jsonify(day), 200)
-            return res
-    return make_response(jsonify({"error": "Date not found"}), 500)
-
-
-@app.route("/schedule", methods=['POST'])
-def add_schedule():
-    req = request.get_json()
-
-    for day in schedule:
-        if str(day["date"]) == str(req["date"]):
-            return make_response(jsonify({"error": "Date already exists"}), 500)
-
-    try:
-        for movie_id in req["movies"]:
-            resp = requests.get(f"http://localhost:3200/movies/{movie_id}")
-            if resp.status_code != 200:
-                return make_response(jsonify({"error": f"Invalid movie ID: {movie_id}"}), 500)
-    except Exception as e:
-        return make_response(jsonify({"error": "Movies service unavailable", "detail": str(e)}), 503)
-
-    new_day = {
-        "date": req["date"],
-        "movies": req["movies"]
-    }
-    schedule.append(new_day)
-    write(schedule)
-    return make_response(jsonify({"message": "Date added"}), 200)
-
-@app.route("/schedule/<date>", methods=['DELETE'])
-def delete_schedule(date):
-    for day in schedule:
-        if str(day["date"]) == str(date):
-            schedule.remove(day)
-            write(schedule)
-            return make_response(jsonify(day), 200)
-    return make_response(jsonify({"error": "Date not found"}), 500)
-
-
-if __name__ == "__main__":
-    print("Server running in port %s" % (PORT))
-    app.run(host=HOST, port=PORT)
+if __name__ == '__main__':
+    serve()
